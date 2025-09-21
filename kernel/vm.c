8e2f18920a2990475a7194d7c2ccada6799d43fb
Li Xunyi <shawn@LideMacBook-Pro.local>
2025-09-21 09:18:47 +0800

    little changes after merge.

--- kernel/vm.c
+++ kernel/vm.c
@@ -336,7 +336,8 @@ uvmcopy(pagetable_t old, pagetable_t new, uint64 sz)
       *pte = *pte | PTE_CW;
       // *pte = *pte | PTE_R;
     }
-   if(mappages(new, i, PGSIZE, (uint64)pa, flags) != 0){
+    flags = PTE_FLAGS(*pte);
+    if(mappages(new, i, PGSIZE, (uint64)pa, flags) != 0){
       goto err;
     }
     ref_page_inc(pa);
